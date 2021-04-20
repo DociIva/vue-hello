968b4b498599fdf334886ac1c1b096253b6b7821
DociIva <doci.iva@hotmail.com>
2021-04-20 16:32:37 +0200

    2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@
  * Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data. 
  */
 
+// 1 
+const app = new Vue({
+    el: '#app',
+    data: {
+        myClasses:'header-title primary',
+        title:'Viaggiare',
+        image:'https://gametimers.it/wp-content/uploads/2020/08/Doctor_Who_Tardis.jpg'
+    }
+});
